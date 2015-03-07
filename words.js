fae7d948190541e18629b309a9da51e205d23aed
Xavier Walcome <walco005@morris.umn.edu>
2015-03-07 14:40:44 -0600

    Delete words.js

--- words.js
+++
@@ -1,4 +0,0 @@
-//Going to insert the different arrays of words here in an array of arrays of words.
-var words = ["these", "words", "are", "just", "some", "stupid", "testing", "random"];
-
-//Max length of words without it looking butt-ugly is 20 letters.
